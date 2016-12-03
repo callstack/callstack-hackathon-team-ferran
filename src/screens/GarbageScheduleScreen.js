8540a08184158c05dfc2f1179b8541041c2ba2eb
Alexander Suevalov <suevalov.work@gmail.com>
2016-12-03 16:04:59 +0100

    Added ability to click on item list

--- src/screens/GarbageScheduleScreen.js
+++ src/screens/GarbageScheduleScreen.js
@@ -2,18 +2,22 @@ import React, {Component} from 'react';
 import { View, Text } from 'react-native';
 import { List, ListItem } from 'react-native-elements'
 import _ from 'lodash';
+import Router from '../router';
 
 import { getPhotoByName } from '../services/imagesService';
 
 export default class GarbageScheduleScreen extends Component {
+
   static route = {
     navigationBar: {
       title: 'Garbage Schedule',
     },
   };
+
   static getSubtitle(from, to) {
     return `from ${from} to ${to}`;
   }
+
   render() {
     const { route: { params } } = this.props;
     const schedule = params.data.schedule;
@@ -31,6 +35,9 @@ export default class GarbageScheduleScreen extends Component {
                 title={l.name}
                 subtitle={GarbageScheduleScreen.getSubtitle(l.from, l.to)}
                 hideChevron
+                onPress={() => {
+                  this.props.navigator.push(Router.getRoute('garbage_monster', { who: l.name }));
+                }}
               />
             ))
           }
@@ -38,4 +45,5 @@ export default class GarbageScheduleScreen extends Component {
       </View>
     );
   }
+
 }
